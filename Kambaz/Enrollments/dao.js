4fc872fe759fccba8671957dcd6fab42cb4dc93f
bsreedee <bsreedee@gmail.com>
2025-12-01 21:07:17 -0500

    Refactor enrollUserInCourse function implementation

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,9 +1,14 @@
 import { v4 as uuidv4 } from "uuid";
 export default function EnrollmentsDao(db) {
+  // function enrollUserInCourse(userId, courseId) {
+  //   const { enrollments } = db;
+  //   enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
+  // }
   function enrollUserInCourse(userId, courseId) {
-    const { enrollments } = db;
-    enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
-  }
+  const enrollment = { _id: uuidv4(), user: userId, course: courseId };
+  db.enrollments.push(enrollment);
+  return enrollment;
+}
   function unenrollUserFromCourse(enrollmentId) {
     const { enrollments } = db;
     db.enrollments = enrollments.filter(
